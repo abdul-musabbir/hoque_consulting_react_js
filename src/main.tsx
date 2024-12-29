34940cda0550056659d8460f530d9984ec18f22c
Abdul Musabbir <musabbir.net@gmail.com>
2024-12-29 14:19:23 +0600

    add service and portfolio page

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./global.css";
 import "./index.css";
+import Service from "./pages/service/Service.tsx";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/services",
+    element: <Service />,
+  },
+  {
+    path: "*",
+    element: <h1>Not Found</h1>,
   },
 ]);
 
